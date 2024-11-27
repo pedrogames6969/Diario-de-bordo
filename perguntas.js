c7048340bf20d6fde79e57a25ea25d08e9f82fe8
pedrogames6969 <146363088+pedrogames6969@users.noreply.github.com>
2024-11-26 23:04:43 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -19,17 +19,17 @@ criaCartao(
 criaCartao(
     'Passeios', 
     'Montego Bay, GreenWood Great House e Main Street Rose Hall, .', 
-    'MoBay como é chamado apresenta muitas praias e natureza, Rose hall é um shopping e o greenwood era a casa de produtores de cana de açucar com cerca de 2000 escravos.'
+    'MoBay como é chamado apresenta muitas praias e natureza, Rose hall é um shopping e o greenwood era a casa de produtores de cana de açucar .'
 )
 
 criaCartao(
     'Prato Típico', 
     'Conhece o "Patties"?', 
-    'o Patties é uma especie de pastel como conhemos aqui no Brasil, porém ele é assado em vez de frito, tabem é acompanhado de muitos temperos na carne, o pattie é um prato simples mas muito saboroso e define muito bem a Jamaica, o seu ingredieten principal é a carne moida'
+    'o Patties é uma especie de pastel, o pattie é um prato simples mas muito saboroso e define a Jamaica, o seu ingrediente principal é a carne moida'
 )
 
 criaCartao(
     'Ponto de Vista', 
     'O que eu achei da viagem para a Jamaica?', 
-    'A viagem foi satisfatoria clima quente comida boa, pessoas alegres e o melhor a musica, mas o pior foieu ter pisado em um prego na Doctor's Cave beach.'
+    'A viagem foi satisfatoria clima quente comida boa, pessoas alegres e o melhor a musica.'
 )
